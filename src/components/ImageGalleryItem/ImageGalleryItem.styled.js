a5a0052936bbd8d6758ded10dc01b46ad18607ff
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2023-09-06 13:39:50 +0300

    f0x3

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.js
@@ -7,6 +7,9 @@ export const GalleryItem = styled.li`
 `;
 
 export const Image = styled.img`
+ display: block;
+//   max-width: 100%;
+  height: auto;
   width: 100%;
   height: 260px;
   object-fit: cover;
